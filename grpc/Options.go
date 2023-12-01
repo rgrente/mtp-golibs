bb8c3f2813f4d6ebb3d1c901e4c6481c8a7c8968
Romain Grente <romain.grente@gmail.com>
2023-12-01 15:30:24 +0100

    refacto + mutual tls option

--- grpc/Options.go
+++ grpc/Options.go
@@ -3,6 +3,7 @@ package grpc
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -18,18 +19,29 @@ func SetGRPCClientOptions() ([]grpc.DialOption, error) {
 	var opts []grpc.DialOption
 
 	// load TLS credentials (CA)
-	tlsCredentials, err := security.LoadTLSCredentials()
+	CACertPool, err := security.SetCACertPool()
 	if err != nil {
-		log.Fatal("cannot load TLS credentials: ", err)
+		log.Fatal("cannot generate CA cert pool: ", err)
 	}
 
-	opts = append(opts, grpc.WithTransportCredentials(tlsCredentials))
+	// Create TLS config
+	config := &tls.Config{
+		RootCAs: CACertPool,
+	}
+
+	// append opts with TLS config credential
+	opts = append(opts, grpc.WithTransportCredentials(credentials.NewTLS(config)))
 
 	return opts, nil
 
 }
 
-func SetGRPCServerOptions() ([]grpc.ServerOption, error) {
+func SetGRPCServerOptions(disableClientAuth bool) ([]grpc.ServerOption, error) {
+
+	// check if SERVER_CRT_LOCATION & SERVER_KEY_LOCATION env vars are set
+	if os.Getenv("SERVER_CRT_LOCATION") == "" || os.Getenv("SERVER_KEY_LOCATION") == "" {
+		return nil, errors.New("SERVER_CRT_LOCATION & SERVER_KEY_LOCATION env vars must be set")
+	}
 
 	// Load server key pair
 	cert, err := tls.LoadX509KeyPair(os.Getenv("SERVER_CRT_LOCATION"), os.Getenv("SERVER_KEY_LOCATION"))
@@ -37,21 +49,31 @@ func SetGRPCServerOptions() ([]grpc.ServerOption, error) {
 		return nil, err
 	}
 
-	// Load CA cert (to check client cert)
-	caCert, err := os.ReadFile(os.Getenv("CA_CRT_LOCATION"))
-	if err != nil {
-		return nil, err
-	}
-	certPool := x509.NewCertPool()
-	if !certPool.AppendCertsFromPEM(caCert) {
-		return nil, fmt.Errorf("failed to add CA's certificate")
-	}
-
-	// Create the credentials and return it
+	// Create TLS config
 	config := &tls.Config{
 		Certificates: []tls.Certificate{cert},
-		ClientAuth:   tls.RequireAndVerifyClientCert,
-		ClientCAs:    certPool,
+	}
+
+	// if client auth enabled
+	if !disableClientAuth {
+
+		// check if CA_CRT_LOCATION env var is set
+		if os.Getenv("CA_CRT_LOCATION") == "" {
+			return nil, errors.New("CA_CRT_LOCATION env var must be set when client auth is enabled")
+		}
+
+		// Load CA cert (to check client cert)
+		caCert, err := os.ReadFile(os.Getenv("CA_CRT_LOCATION"))
+		if err != nil {
+			return nil, err
+		}
+		certPool := x509.NewCertPool()
+		if !certPool.AppendCertsFromPEM(caCert) {
+			return nil, fmt.Errorf("failed to add CA's certificate")
+		}
+
+		config.ClientCAs = certPool
+		config.ClientAuth = tls.RequireAndVerifyClientCert
 	}
 
 	// set TLS option
--- grpc/Options_test.go
+++ grpc/Options_test.go
@@ -1,6 +1,8 @@
 package grpc
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSetGRPCClientOptions(t *testing.T) {
 	t.Setenv("CA_CRT_LOCATION", "../tests/ca.crt")
@@ -11,9 +13,69 @@ func TestSetGRPCClientOptions(t *testing.T) {
 }
 
 func TestSetGRPCServerOptions(t *testing.T) {
+
+	// no env vars + disable client auth
+	_, err := SetGRPCServerOptions(true)
+	if err.Error() != "SERVER_CRT_LOCATION & SERVER_KEY_LOCATION env vars must be set" {
+		t.Errorf(err.Error())
+	}
+
+	// no env vars + enable client auth
+	_, err = SetGRPCServerOptions(false)
+	if err.Error() != "SERVER_CRT_LOCATION & SERVER_KEY_LOCATION env vars must be set" {
+		t.Errorf(err.Error())
+	}
+
+	t.Setenv("SERVER_CRT_LOCATION", "../tests/tl.crt")
+	t.Setenv("SERVER_KEY_LOCATION", "../tests/tls.key")
+	// wrong env vars path + disable client auth
+	_, err = SetGRPCServerOptions(true)
+	if err.Error() != "open ../tests/tl.crt: no such file or directory" {
+		t.Errorf(err.Error())
+	}
+
+	// wrong env vars path + enable client auth
+	_, err = SetGRPCServerOptions(false)
+	if err.Error() != "open ../tests/tl.crt: no such file or directory" {
+		t.Errorf(err.Error())
+	}
+
 	t.Setenv("SERVER_CRT_LOCATION", "../tests/tls.crt")
 	t.Setenv("SERVER_KEY_LOCATION", "../tests/tls.key")
-	_, err := SetGRPCServerOptions()
+	// server env vars + disable client auth
+	_, err = SetGRPCServerOptions(true)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// server env vars + enable client auth
+	_, err = SetGRPCServerOptions(false)
+	if err.Error() != "CA_CRT_LOCATION env var must be set when client auth is enabled" {
+		t.Errorf(err.Error())
+	}
+
+	t.Setenv("CA_CRT_LOCATION", "../tests/c.crt")
+	// all env vars + disable client auth
+	_, err = SetGRPCServerOptions(true)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// all env vars + enable client auth
+	_, err = SetGRPCServerOptions(false)
+	if err.Error() != "open ../tests/c.crt: no such file or directory" {
+		t.Errorf(err.Error())
+	}
+
+	t.Setenv("CA_CRT_LOCATION", "../tests/ca.crt")
+	// all env vars + disable client auth
+	_, err = SetGRPCServerOptions(true)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	// all env vars + enable client auth
+	_, err = SetGRPCServerOptions(false)
 	if err != nil {
 		t.Errorf(err.Error())
 	}
