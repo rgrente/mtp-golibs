b061c620c4601ee8aea2297d183b41caf9f5c39f
joseph.delespinay.ext <joseph.delespinay.ext@orange.com>
2024-01-08 01:23:48 +0100

    feat: otel init + otelgin

---
+++ otel/gin/tracer.go
@@ -0,0 +1,59 @@
+package motelgin
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"google.golang.org/grpc/credentials"
+)
+
+var (
+	serviceName  = "frontend"
+	collectorURL = os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT") // address of OTLP exporter endpoint -> Grafana, jaeger etc...
+	insecure     = os.Getenv("INSECURE_MODE")               // GRPC communcation security -> to confirm if it's between API and exporter or other
+)
+
+func initTracer() func(context.Context) error {
+	secureOption := otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
+	if len(insecure) > 0 {
+		secureOption = otlptracegrpc.WithInsecure()
+	}
+
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			secureOption,
+			otlptracegrpc.WithEndpoint(collectorURL),
+		),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	resources, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			attribute.String("service.name", serviceName),
+			attribute.String("library.language", "go"),
+		),
+	)
+	if err != nil {
+		log.Printf("Could not set resources: ", err)
+	}
+
+	otel.SetTracerProvider(
+		sdktrace.NewTracerProvider(
+			sdktrace.WithSampler(sdktrace.AlwaysSample()),
+			sdktrace.WithBatcher(exporter),
+			sdktrace.WithResource(resources),
+		),
+	)
+	return exporter.Shutdown
+}
