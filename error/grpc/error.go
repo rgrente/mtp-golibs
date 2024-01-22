75b54b8dba22fe4ab11f2db5ddcadfc765ca1d12
joseph.delespinay.ext <joseph.delespinay.ext@orange.com>
2024-01-22 23:08:42 +0100

    feat: error management

---
+++ error/grpc/error.go
@@ -0,0 +1,58 @@
+package grpc
+
+import (
+	"context"
+	"errors"
+	"runtime"
+	"strconv"
+	"strings"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	me "github.com/rgrente/mtp-golibs/merror"
+)
+
+func processGRPCError(ctx context.Context, e error) error {
+	var err *me.MError
+	var trailer metadata.MD
+	var trace string
+	serviceName, ok := os.LookupEnv("SERVICE_NAME")
+	pc, file, line, callerOk := runtime.Caller(1)
+
+	switch {
+	case errors.As(e, &err):
+		if !ok {
+			if err.trace == "" && callerOk {
+				trace = file[strings.LastIndex(file, "/")+1:] +
+				":" + runtime.FuncForPC(pc).Name() +
+				":" + strconv.Itoa(line)
+			} else {
+				trace = err.trace
+			}
+		} else {
+			trace = serviceName+"/"+err.trace
+		}
+	default:
+		err = err.New(e.Error())
+		if !ok {
+			trace = ""
+		} else {
+			trace = serviceName
+		}
+	}
+	trailer = metadata.Pairs(
+		"code", strconv.Itoa(err.code),
+		"message", err.message,
+		"description", err.description,
+		"trace", trace,
+		"dev_code", strconv.Itoa(err.devCode),
+		"dev_message", err.devMessage,
+		"dev_description", err.devDescription,
+	)
+
+	grpc.SetTrailer(ctx, trailer)
+	return status.Error(codes.Aborted, err.message)
+}
---
+++ error/http/error.go
@@ -0,0 +1,76 @@
+package http
+
+import (
+	"fmt"
+	"strconv"
+	"errors"
+	"encoding/json"
+
+	"github.com/gin-gonic/gin"
+
+	"google.golang.org/grpc/metadata"
+	me "github.com/rgrente/mtp-golibs/merror"
+)
+
+type MultipassError struct {
+	Code        int    `json:"code"`
+	Message     string `json:"message"`
+	Description string `json:"description"`
+}
+
+// Raise HTTP Fail with specified status
+// Send HTTP error message
+// Log Developer error message (with more detailed error)
+func renderError(c *gin.Context, err error) {
+	var mError *me.MError
+	HTTPError := MultipassError{}
+	colorYellow := "\u001b[33m"
+	colorNone := "\033[0m"
+
+	switch {
+	case errors.As(err, &mError):
+		// Raise HTTP status and render HTTP error
+		HTTPError.Code = mError.code
+		HTTPError.Message = mError.message
+		HTTPError.Description = mError.description
+		c.AbortWithStatus(mError.code)
+		errorJSON, _ := json.Marshal(HTTPError)
+		c.Data(mError.code, "application/json", errorJSON)
+
+		// Log error for developer
+		fmt.Println(string(colorYellow), "Developer Error:")
+		fmt.Println("Code: ", mError.devCode)
+		fmt.Println("Message: ", mError.devMessage)
+		fmt.Println("Description: ", mError.devDescription)
+		fmt.Println("Trace: ", mError.trace, string(colorNone))
+	default:
+		c.AbortWithStatus(501)
+		HTTPError.Code = 501
+		HTTPError.Message = "Unknown Error"
+		HTTPError.Description = ""
+	}
+}
+
+func toMError(trailer metadata.MD) error {
+	code, err := strconv.Atoi(trailer["code"][0])
+	mError := &me.MError{}
+	if err != nil {
+		mError.code = 422
+	} else {
+		mError.code = code
+	}
+	devCode, err := strconv.Atoi(trailer["dev_code"][0])
+	if err != nil {
+		mError.devCode = 501
+	} else {
+		mError.devCode = devCode
+	}
+
+	mError.message = trailer["message"][0]
+	mError.description = trailer["description"][0]
+	mError.trace = trailer["trace"][0]
+	mError.devMessage = trailer["dev_message"][0]
+	mError.devDescription = trailer["dev_description"][0]
+
+	return mError
+}
