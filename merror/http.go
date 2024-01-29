770ad039208f9f70c1dad66869e2029e4deb802e
joseph.delespinay.ext <joseph.delespinay.ext@orange.com>
2024-01-29 11:07:32 +0100

    feat: comment merror package

--- merror/http.go
+++ merror/http.go
@@ -1,3 +1,5 @@
+// Package merror provides utilities for handling and rendering errors in a Gin-based application.
+
 package merror
 
 import (
@@ -12,24 +14,25 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
+// MultipassError represents a structured error format with code, message, and description.
 type MultipassError struct {
 	Code        int    `json:"code"`
 	Message     string `json:"message"`
 	Description string `json:"description"`
 }
 
-// Raise HTTP Fail with specified status
-// Send HTTP error message
-// Log Developer error message (with more detailed error)
+// RenderError handles and renders errors in the Gin context.
+// It sets the HTTP status, sends an HTTP error message, and logs developer error details.
 func RenderError(c *gin.Context, err error) {
 	var mError *MError
 	HTTPError := MultipassError{}
 	colorYellow := "\u001b[33m"
 	colorNone := "\033[0m"
 
+	// Check if the error is of type MError.
 	switch {
 	case errors.As(err, &mError):
-		// Raise HTTP status and render HTTP error
+		// If it is, set HTTP status and render HTTP error.
 		HTTPError.Code = 422
 		HTTPError.Message = mError.Message
 		HTTPError.Description = mError.Description
@@ -37,7 +40,7 @@ func RenderError(c *gin.Context, err error) {
 		errorJSON, _ := json.Marshal(HTTPError)
 		c.Data(mError.Code, "application/json", errorJSON)
 
-		// Log error for developer
+		// Log error for developer.
 		fmt.Println(string(colorYellow), "Developer Error:")
 		fmt.Println("Raised at: ", time.Now().String())
 		fmt.Println("Code: ", mError.DevCode)
@@ -45,6 +48,7 @@ func RenderError(c *gin.Context, err error) {
 		fmt.Println("Description: ", mError.DevDescription)
 		fmt.Println("Trace: ", mError.Trace, string(colorNone))
 	default:
+		// If not, set a generic 501 status and render an unknown error.
 		c.AbortWithStatus(501)
 		HTTPError.Code = 501
 		HTTPError.Message = "Unknown Error"
@@ -52,21 +56,25 @@ func RenderError(c *gin.Context, err error) {
 	}
 }
 
+// ToMError converts gRPC metadata trailer to a MultipassError.
 func ToMError(trailer metadata.MD) error {
 	code, err := strconv.Atoi(trailer["code"][0])
 	mError := &MError{}
 	if err != nil {
+		// If code conversion fails, set a default code.
 		mError.Code = 422
 	} else {
 		mError.Code = code
 	}
 	devCode, err := strconv.Atoi(trailer["dev_code"][0])
 	if err != nil {
+		// If dev_code conversion fails, set a default dev_code.
 		mError.DevCode = 501
 	} else {
 		mError.DevCode = devCode
 	}
 
+	// Populate MError fields from gRPC metadata.
 	mError.Message = trailer["message"][0]
 	mError.Description = trailer["description"][0]
 	mError.Trace = trailer["trace"][0]
